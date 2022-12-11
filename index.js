19a7b5494e12a141d3a90760c9c162c77bdb6b57
lalitha <20b01a1260@svecw.edu.in>
2022-12-11 19:09:55 +0530

    HTTP SERVER Level 2

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-console.log("hello node js");
 const http = require("http");
 const fs = require("fs");
 
 let homeContent = "";
 let projectContent = "";
-
+let registrationContent = "";
+let args = require("minimist")(process.argv.slice(2));
+// port=port._;
+let port = args["port"];
+// console.log(port)
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
   }
   homeContent = home;
+  // console.log("hii")
 });
 
 fs.readFile("project.html", (err, project) => {
@@ -18,29 +22,36 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
+
 fs.readFile("registration.html", (err, registration) => {
   if (err) {
     throw err;
   }
   registrationContent = registration;
 });
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      case "/project/registration":
-        response.write(registrationContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(3000);
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer((request, response) => {
+      let url = request.url;
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      switch (url) {
+        case "/project":
+          response.write(projectContent);
+          response.end();
+          break;
+        case "/project/registration":
+          response.write(registrationContent);
+          response.end();
+          break;
+        default:
+          response.write(homeContent);
+          response.end();
+          break;
+      }
+    })
+    .listen(port);
+});
